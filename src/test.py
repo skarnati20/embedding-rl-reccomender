dbfc3737985709046a9db370a28f7ef5931a9295
skarnati20 <73152445+skarnati20@users.noreply.github.com>
2025-05-05 21:42:12 -0400

    Fix testing and remove pycache

--- src/test.py
+++ src/test.py
@@ -5,17 +5,17 @@ rec = Reccomender("../data/combined.csv")
 
 def generate_reward(filename):
     if filename.startswith("5"):
-        return np.random.choice([0, 10], p=[0.9, 0.1])
+        return np.random.choice([0, 20], p=[0.9, 0.1])
     elif filename.startswith("3"):
-        return np.random.choice([0, 10], p=[0.9, 0.1])
+        return np.random.choice([0, 20], p=[0.4, 0.6])
     elif filename.startswith("10"):
-        return np.random.choice([0, 10], p=[0.1, 0.9])
+        return np.random.choice([0, 20], p=[0.1, 0.9])
     else:
         return 0
 
 # Preference phase
 preference_counts = {"atheism": 0, "graphics": 0, "baseball": 0}
-for _ in range(500):
+for _ in range(1000):
     file, embedding = rec.recommend_article()
     if file.startswith("5"):
         preference_counts["atheism"] += 1
@@ -26,15 +26,3 @@ for _ in range(500):
     print(preference_counts)
     reward = generate_reward(file)
     rec.register_reaction(embedding, reward)
-
-counts = {"atheism": 0, "graphics": 0, "baseball": 0}
-for _ in range(50):
-    file, _ = rec.recommend_article()
-    if file.startswith("5"):
-        counts["atheism"] += 1
-    elif file.startswith("3"):
-        counts["graphics"] += 1
-    elif file.startswith("10"):
-        counts["baseball"] += 1
-
-print(counts)
